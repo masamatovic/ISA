8baafb116dad55ebd60c9d61a07146c8520bcd65
Masa Matovic <masa.matovic97@gmail.com>
2020-04-10 18:42:49 +0200

    Hello World metoda za front

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jdk.net.SocketFlow;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
+@RequestMapping(value = "/pozdrav")
 public class HelloWorld {
 
-    @RequestMapping("/")
-    public String pozdrav(){
-        return "Hello World";
+   @GetMapping(value = "/masa")
+    public ResponseEntity<String> pozdrav(){
+        String ime = "masa";
+        return new ResponseEntity<>(ime, HttpStatus.OK) ;
     }
 
 }
